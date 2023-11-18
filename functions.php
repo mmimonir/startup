62f782482cab3bab50aeb988279f10402ffc5d59
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2023-11-19 01:29:44 +0600

    class 15, 16 completed

--- functions.php
+++ functions.php
@@ -334,8 +334,7 @@ function startup_sidebar()
 
 add_action('widgets_init', 'startup_sidebar');
 
-// Creating the widget
-// Creating the widget
+// Startup Search Widget
 class startup_search_widget extends WP_Widget
 {
 
@@ -391,7 +390,7 @@ class startup_search_widget extends WP_Widget
             <p>
                 <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
                 <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
-    <?php
+            <?php
         }
     }
 
@@ -413,3 +412,166 @@ function startup_search_widget_load()
 }
 
 add_action('widgets_init', 'startup_search_widget_load');
+
+// Startup Category Widget
+class startup_category_widget extends WP_Widget
+{
+
+    // The construct part
+    function __construct()
+    {
+        parent::__construct(
+            // Base ID of your widget
+            'startup_category_widget',
+
+            // Widget name will appear in UI
+            __('Startup Category Widget', 'startup'),
+
+            // Widget description
+            array('description' => __('Search Widget for Startup Theme', 'startup'),)
+        );
+    }
+
+    // Creating widget front-end
+    public function widget($args, $instance)
+    {
+        $title = apply_filters('widget_title', $instance['title']);
+        echo $args['before_widget'];
+        if (!empty($title)) {
+            echo $args['before_title'] . $title . $args['after_title'];
+            ?>
+                <!-- Category Start -->
+            <div class="link-animated d-flex flex-column justify-content-start">
+                <?php
+                $cats = get_categories();
+                foreach ($cats as $cat) {
+                ?>
+                    <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="<?php echo get_category_link($cat->term_id); ?>"><i class="fa fa-arrow-right me-2"></i><?php echo $cat->name; ?> - <?php echo $cat->count; ?></a>
+                <?php
+                }
+                ?>
+            </div>
+            <!-- Category End -->
+        <?php
+            echo $args['after_widget'];
+        }
+    }
+
+    // Creating widget Backend
+    public function form($instance)
+    {
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = __('Categories', 'startup');
+        ?>
+
+            <p>
+                <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+                <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+                <?php
+            }
+        }
+
+        // Updating widget replacing old instances with new
+        public function update($new_instance, $old_instance)
+        {
+            $instance = array();
+            $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+            return $instance;
+        }
+
+        // Class wpb_widget ends here
+    }
+
+    // Register and load the widget
+    function startup_category_widget_load()
+    {
+        register_widget('startup_category_widget');
+    }
+
+    add_action('widgets_init', 'startup_category_widget_load');
+
+    // Startup Recent Post Widget
+    class startup_recent_widget extends WP_Widget
+    {
+
+        // The construct part
+        function __construct()
+        {
+            parent::__construct(
+                // Base ID of your widget
+                'startup_recent_widget',
+
+                // Widget name will appear in UI
+                __('Startup Recent Post Widget', 'startup'),
+
+                // Widget description
+                array('description' => __('Search Widget for Startup Theme', 'startup'),)
+            );
+        }
+
+        // Creating widget front-end
+        public function widget($args, $instance)
+        {
+            $title = apply_filters('widget_title', $instance['title']);
+            echo $args['before_widget'];
+
+            if (!empty($title)) {
+                echo $args['before_title'] . $title . $args['after_title'];
+                $arg = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => 5,
+                );
+                $query = new WP_Query($arg);
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                ?>
+            <div class="d-flex rounded overflow-hidden mb-3">
+                <img class="img-fluid" src="<?php the_post_thumbnail_url(); ?>" style="width: 100px; height: 100px; object-fit: cover;" alt="">
+                <a href="<?php the_permalink(); ?>" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0"><?php the_title();  ?>
+                </a>
+            </div>
+    <?php
+                    }
+                    wp_reset_postdata();
+                }
+                echo $args['after_widget'];
+            }
+        }
+
+        // Creating widget Backend
+        public function form($instance)
+        {
+            if (isset($instance['title'])) {
+                $title = $instance['title'];
+            } else {
+                $title = __('Categories', 'startup');
+    ?>
+
+    <p>
+        <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+<?php
+            }
+        }
+
+        // Updating widget replacing old instances with new
+        public function update($new_instance, $old_instance)
+        {
+            $instance = array();
+            $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+            return $instance;
+        }
+
+        // Class wpb_widget ends here
+    }
+
+    // Register and load the widget
+    function startup_recent_post_widget_load()
+    {
+        register_widget('startup_recent_widget');
+    }
+
+    add_action('widgets_init', 'startup_recent_post_widget_load');
--- sidebar.php
+++ sidebar.php
@@ -4,66 +4,6 @@ if (is_active_sidebar('main-sidebar')) {
 }
 ?>
 
-
-<!-- Category Start -->
-<div class="mb-5 wow slideInUp" data-wow-delay="0.1s">
-    <div class="section-title section-title-sm position-relative pb-3 mb-4">
-        <h3 class="mb-0">Categories</h3>
-    </div>
-    <div class="link-animated d-flex flex-column justify-content-start">
-        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Web Design</a>
-        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Web Development</a>
-        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Web Development</a>
-        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Keyword Research</a>
-        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Email Marketing</a>
-    </div>
-</div>
-<!-- Category End -->
-
-<!-- Recent Post Start -->
-<div class="mb-5 wow slideInUp" data-wow-delay="0.1s">
-    <div class="section-title section-title-sm position-relative pb-3 mb-4">
-        <h3 class="mb-0">Recent Post</h3>
-    </div>
-    <div class="d-flex rounded overflow-hidden mb-3">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-1.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-        </a>
-    </div>
-    <div class="d-flex rounded overflow-hidden mb-3">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-2.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-        </a>
-    </div>
-    <div class="d-flex rounded overflow-hidden mb-3">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-3.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-        </a>
-    </div>
-    <div class="d-flex rounded overflow-hidden mb-3">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-1.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-        </a>
-    </div>
-    <div class="d-flex rounded overflow-hidden mb-3">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-2.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-        </a>
-    </div>
-    <div class="d-flex rounded overflow-hidden mb-3">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-3.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-        </a>
-    </div>
-</div>
-<!-- Recent Post End -->
-
-<!-- Image Start -->
-<div class="mb-5 wow slideInUp" data-wow-delay="0.1s">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-1.jpg" alt="" class="img-fluid rounded">
-</div>
-<!-- Image End -->
-
 <!-- Tags Start -->
 <div class="mb-5 wow slideInUp" data-wow-delay="0.1s">
     <div class="section-title section-title-sm position-relative pb-3 mb-4">
